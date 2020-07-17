38c10f56f1fc75971fa83c65b9a2849f6dec07c9
valviveros <blkfthrs@gmail.com>
2020-07-17 10:06:30 -0500

    Biblioteca y lago listos
     On branch dev1
     Changes to be committed:
    	modified:   .DS_Store
    	new file:   Assets/.DS_Store
    	new file:   Assets/Animations.meta
    	new file:   Assets/Animations/Animated Tiles.meta
    	new file:   Assets/Animations/Animated Tiles/FadeFromBlack.anim
    	new file:   Assets/Animations/Animated Tiles/FadeFromBlack.anim.meta
    	new file:   Assets/Animations/Animated Tiles/FadeToBlack.anim
    	new file:   Assets/Animations/Animated Tiles/FadeToBlack.anim.meta
    	new file:   Assets/Animations/Animated Tiles/Panel 1.controller
    	new file:   Assets/Animations/Animated Tiles/Panel 1.controller.meta
    	new file:   Assets/Animations/Animated Tiles/Panel 2.controller
    	new file:   Assets/Animations/Animated Tiles/Panel 2.controller.meta
    	new file:   Assets/Animations/Animated Tiles/Panel.controller
    	new file:   Assets/Animations/Animated Tiles/Panel.controller.meta
    	new file:   Assets/Animations/Animated Tiles/Water01.asset
    	new file:   Assets/Animations/Animated Tiles/Water01.asset.meta
    	new file:   Assets/Animations/Animated Tiles/Water02.asset
    	new file:   Assets/Animations/Animated Tiles/Water02.asset.meta
    	new file:   Assets/Animations/Animated Tiles/Water03.asset
    	new file:   Assets/Animations/Animated Tiles/Water03.asset.meta
    	new file:   Assets/Animations/Animated Tiles/Water04.asset
    	new file:   Assets/Animations/Animated Tiles/Water04.asset.meta
    	new file:   Assets/Animations/Animated Tiles/Water05.asset
    	new file:   Assets/Animations/Animated Tiles/Water05.asset.meta
    	new file:   Assets/Animations/Animated Tiles/Water06.asset
    	new file:   Assets/Animations/Animated Tiles/Water06.asset.meta
    	new file:   Assets/Animations/Animated Tiles/Water07.asset
    	new file:   Assets/Animations/Animated Tiles/Water07.asset.meta

---
+++ Assets/Scripts/PlayerItems.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerItems : MonoBehaviour
+{
+    public bool map;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/WhereAmI.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhereAmI : MonoBehaviour
+{
+    public GameObject room;
+    static public Vector2 maxPosition;
+    static public Vector2 minPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player") && room.name == "WhereAmI1")
+        {
+            Debug.Log("Estoy en la Room1");
+            maxPosition.x = 2.28f;
+            maxPosition.y = 10f;
+            minPosition.x = -10.31f;
+            minPosition.y = -1.93f;
+        } else if (col.CompareTag("Player") && room.name == "WhereAmI2")
+        {
+            Debug.Log("Estoy en la Room2");
+            maxPosition.x = 2.28f;
+            maxPosition.y = 39.96f;
+            minPosition.x = -10.31f;
+            minPosition.y = 20f;
+        }else if (col.CompareTag("Player") && room.name == "WhereAmI3")
+        {
+            Debug.Log("Estoy en la Room3");
+            maxPosition.x = 37.29f;
+            maxPosition.y = 68.96f;
+            minPosition.x = 15.71f;
+            minPosition.y = -1.93f;
+        } else if (col.CompareTag("Player") && room.name == "WhereAmIBiblio")
+        {
+            Debug.Log("Estoy en la Biblio1");
+            maxPosition.x = 9.2f;
+            maxPosition.y = 4.8f;
+            minPosition.x = -0.21f;
+            minPosition.y = -9.21f;
+        } else if (col.CompareTag("Player") && room.name == "WhereAmIBiblio2F")
+        {
+            Debug.Log("Estoy en la Biblio2");
+            maxPosition.x = 9.2f;
+            maxPosition.y = 4.88f;
+            minPosition.x = -0.19f;
+            minPosition.y = -11.85f;
+        } else if (col.CompareTag("Player") && room.name == "WhereAmIBiblio3F")
+        {
+            Debug.Log("Estoy en la Biblio3");
+            maxPosition.x = 9.13f;
+            maxPosition.y = 4.76f;
+            minPosition.x = -0.18f;
+            minPosition.y = -3.88f;
+        }
+    }
+}
